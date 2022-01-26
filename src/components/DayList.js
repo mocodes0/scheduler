9bbfa87337f7a2718464b79ca406955991b51341
Mohamed Bashir <Moe@MacBook-Air.local>
2022-01-26 15:54:24 -0500

    fix: updated on change and props  for spots

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,13 +3,14 @@ import DayListItem from './DayListItem.js';
 
 export default function DayList(props) {
   const days = props.days.map(day => {
+    console.log(day, 'day')
     return (
       <DayListItem
       key={day.id}
       name={day.name}
       spots={day.spots}
       selected={day.name === props.value}
-      setDay={props.setDay}
+      setDay={props.onChange}
       />
     );
   });
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,7 @@ import './DayListItem.scss';
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  console.log(props)
 
   const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0});
   const formatSpots = (spots) => {
@@ -19,7 +20,7 @@ export default function DayListItem(props) {
     return (
       <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
         <h2 className="text--regular">{props.name}</h2> 
-        <h3 className="text--light">{formatSpots()}</h3>
+        <h3 className="text--light">{formatSpots(props.spots)}</h3>
       </li>
     );
 
